6592c1ca5739c6ef1f09dd30a272b2cce050631c
kshitij <kpandey0022@gmail.com>
2026-06-08 10:44:25 +0530

    Commit

--- leetcode/1_easy/LinkedList/201_Optimal.cpp
+++ leetcode/1_easy/LinkedList/201_Optimal.cpp
@@ -3,13 +3,13 @@ public:
     ListNode* reverseList(ListNode* head) {
         //using 3 pointers
 
-        ListNode* curr=head;
+        ListNode* temp=head;
         ListNode* prev=NULL;
-        while(curr!=NULL){
-            ListNode* temp=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=temp;
+        while(temp!=NULL){
+            ListNode* front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
         }
         return prev;
     }
